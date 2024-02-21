7b6d30caa77bbe1809c17fd6ed3ca24b19243501
lucas mathias <131968336+lucasmathiasbelini@users.noreply.github.com>
2024-02-21 09:30:36 -0300

    c sharp

---
+++ hahaha/hahaha/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace hahaha;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
